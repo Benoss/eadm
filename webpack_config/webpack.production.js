c4fd658ce609825d9c0ea4a749c2690bf28373ec
Ben Chab <benauf@gmail.com>
2015-06-07 14:59:19 +1200

    Fix folder issue

--- webpack_config/webpack.production.js
+++ webpack_config/webpack.production.js
@@ -17,7 +17,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.join(__dirname, '..', '_site'),
+    path: path.join(__dirname, '..', 'dist'),
     publicPath: './',
     filename: '[name].js'
   },
