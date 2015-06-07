528bfb29394326fe956c5fe84bc7ba917a6e01f0
Ben Chab <benauf@gmail.com>
2015-06-07 13:55:17 +1200

    Fix icons path for gh-pages

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '..', 'build'),
-    publicPath: '/build/',
+    publicPath: './',
     filename: '[name].js'
   },
   jshint: {
